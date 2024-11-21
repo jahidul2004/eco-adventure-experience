83dccdbbcea592a75d1a6397b352bee277a2c527
jahidul2004 <islamjahiduljihad@gmail.com>
2024-11-21 12:47:39 +0600

    Registration component updated.

--- src/components/Registration.jsx
+++ src/components/Registration.jsx
@@ -1,57 +1,61 @@
-import { useContext } from "react";
-import { Link } from "react-router-dom";
+import { useContext, useState } from "react";
+import { Link, Navigate } from "react-router-dom";
 import { AuthContext } from "../provider/AuthProvider";
 import { Helmet } from "react-helmet";
+import { notify } from "../utilities/utils";
 
 const Registration = () => {
-    const { createNewUser, setUser, updateUserProfile } = useContext(AuthContext);
+    const { createNewUser, setUser, updateUserProfile } =
+        useContext(AuthContext);
+    const [redirect, setRedirect] = useState(false);
 
-    const handleSubmit = (event) => {
+    const handleSubmit = async (event) => {
         event.preventDefault();
 
         const formData = new FormData(event.target);
-
         const name = formData.get("name");
         const email = formData.get("email");
         const photo = formData.get("photo");
         const password = formData.get("password");
 
-        createNewUser(email, password)
-        .then((userCredential) => {
-            console.log("User is created: ", userCredential);
-            setUser(userCredential.user);
+        if (!name || !email || !photo || !password) {
+            notify("All fields are required!");
+            return;
+        }
 
-            updateUserProfile({ displayName: name, photoURL: photo })
-            .then((result) =>{
-                console.log("Profile Updated: ", result);
-            })
-            .catch((error) =>{
-                console.error("Error updating profile: ", error);
-            })
-        })
-        .catch((error) => {
-            console.error("Error creating user: ", error);
-        });
+        try {
+            const userCredential = await createNewUser(email, password);
+            const newUser = userCredential.user;
+
+            await updateUserProfile({ displayName: name, photoURL: photo });
+
+            setUser({ ...newUser, displayName: name, photoURL: photo });
+
+            notify("User is created and logged in successfully!");
+            setRedirect(true);
+        } catch (error) {
+            console.error("Error:", error);
+            notify(`ERROR: ${error.message}`);
+        }
     };
 
+    if (redirect) {
+        return <Navigate to="/" />;
+    }
+
     return (
-        <form onSubmit={handleSubmit} className="max-w-[400px] mx-auto shadow-xl p-5 rounded-lg my-10 flex flex-col gap-4">
+        <form
+            onSubmit={handleSubmit}
+            className="max-w-[400px] mx-auto shadow-xl p-5 rounded-lg my-10 flex flex-col gap-4"
+        >
             <Helmet>
                 <title>Registration</title>
             </Helmet>
             <h1 className="text-success text-center font-bold text-2xl">
                 User Registration
             </h1>
+
             <label className="input input-bordered flex items-center gap-2">
-                <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 16 16"
-                    fill="currentColor"
-                    className="h-4 w-4 opacity-70"
-                >
-                    <path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" />
-                    <path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
-                </svg>
                 <input
                     name="name"
                     type="text"
@@ -59,16 +63,8 @@ const Registration = () => {
                     placeholder="Name"
                 />
             </label>
+
             <label className="input input-bordered flex items-center gap-2">
-                <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 16 16"
-                    fill="currentColor"
-                    className="h-4 w-4 opacity-70"
-                >
-                    <path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" />
-                    <path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
-                </svg>
                 <input
                     name="email"
                     type="email"
@@ -76,35 +72,17 @@ const Registration = () => {
                     placeholder="Email"
                 />
             </label>
+
             <label className="input input-bordered flex items-center gap-2">
-                <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 16 16"
-                    fill="currentColor"
-                    className="h-4 w-4 opacity-70"
-                >
-                    <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
-                </svg>
                 <input
                     name="photo"
                     type="text"
                     className="grow"
-                    placeholder="Photo Url"
+                    placeholder="Photo URL"
                 />
             </label>
+
             <label className="input input-bordered flex items-center gap-2">
-                <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 16 16"
-                    fill="currentColor"
-                    className="h-4 w-4 opacity-70"
-                >
-                    <path
-                        fillRule="evenodd"
-                        d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
-                        clipRule="evenodd"
-                    />
-                </svg>
                 <input
                     name="password"
                     type="password"
@@ -112,10 +90,12 @@ const Registration = () => {
                     placeholder="Password"
                 />
             </label>
+
             <button className="btn btn-success text-white">Register</button>
+
             <p>
-                Have any account? please{" "}
-                <Link className="text-success font-semibold" to={"/login"}>
+                Already have an account?{" "}
+                <Link className="text-success font-semibold" to="/login">
                     Login
                 </Link>
             </p>
--- src/components/UpdateProfile.jsx
+++ src/components/UpdateProfile.jsx
@@ -1,10 +1,12 @@
 import { useContext } from "react";
 import { AuthContext } from "../provider/AuthProvider";
+import { Helmet } from "react-helmet";
 import { Navigate } from "react-router-dom";
 import { notify } from "../utilities/utils";
-import { Helmet } from "react-helmet";
 
 const UpdateProfile = () => {
+    const { user, updateUserProfile, setUser } = useContext(AuthContext);
+
     const handleSubmit = (event) => {
         event.preventDefault();
 
@@ -14,8 +16,10 @@ const UpdateProfile = () => {
         const photo = formData.get("photo");
 
         updateUserProfile({ displayName: name, photoURL: photo })
-            .then((result) => {
-                console.log("Profile Updated: ", result);
+            .then(() => {
+                const updatedUser = { ...user, displayName: name, photoURL: photo };
+                setUser(updatedUser);
+
                 notify("Profile Updated Successfully");
             })
             .catch((error) => {
@@ -23,8 +27,6 @@ const UpdateProfile = () => {
                 notify("Error updating profile", "error");
             });
     };
-
-    const { user, updateUserProfile } = useContext(AuthContext);
 
     return (
         <div>
@@ -40,36 +42,21 @@ const UpdateProfile = () => {
                         Update Profile
                     </h1>
                     <label className="input input-bordered flex items-center gap-2">
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 16 16"
-                            fill="currentColor"
-                            className="h-4 w-4 opacity-70"
-                        >
-                            <path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" />
-                            <path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
-                        </svg>
                         <input
                             name="photo"
                             type="text"
                             className="grow"
                             placeholder="Photo URL"
+                            defaultValue={user?.photoURL || ""}
                         />
                     </label>
                     <label className="input input-bordered flex items-center gap-2">
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 16 16"
-                            fill="currentColor"
-                            className="h-4 w-4 opacity-70"
-                        >
-                            <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
-                        </svg>
                         <input
                             name="name"
                             type="text"
                             className="grow"
                             placeholder="Name"
+                            defaultValue={user?.displayName || ""}
                         />
                     </label>
                     <button className="btn btn-success text-white">
