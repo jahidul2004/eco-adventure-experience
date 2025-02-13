90493fb4e123f434814154e8f254b0f4772b30eb
jahidul2004 <islamjahiduljihad@gmail.com>
2025-02-13 19:51:49 +0600

    Footer updated.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,28 +7,24 @@ const Footer = () => {
     return (
         <footer className="footer footer-center bg-[#f7f7f7] p-10">
             <aside>
-                <img
-                    className="w-[80px] h-[80px]"
-                    src={adventureImg}
-                    alt=""
-                />
+                <img className="w-[80px] h-[80px]" src={adventureImg} alt="" />
                 <p className="font-bold text-2xl">Eco-Adventure Experiences</p>
                 <p>
                     Copyright © {new Date().getFullYear()} - All right reserved
                 </p>
             </aside>
             <nav>
-                <div className="grid grid-flow-col gap-4">
-                    <a className="animate__animated animate__rubberBand animate__infinite" href="https://github.com/jahidul2004">
+                <div className="grid grid-flow-col gap-4 text-xl">
+                    <a href="https://github.com/jahidul2004">
                         <FaGithub />
                     </a>
-                    <a className="animate__animated animate__rubberBand animate__infinite" href="https://www.facebook.com/share/dbMWtFD4uzSn8KqU/">
+                    <a href="https://www.facebook.com/share/dbMWtFD4uzSn8KqU/">
                         <FaFacebook />
                     </a>
-                    <a className="animate__animated animate__rubberBand animate__infinite" href="https://www.instagram.com/islam.jahidul.jihad">
+                    <a href="https://www.instagram.com/islam.jahidul.jihad">
                         <FaSquareInstagram />
                     </a>
-                    <a className="animate__animated animate__rubberBand animate__infinite" href="https://www.threads.net/@islam.jahidul.jihad">
+                    <a href="https://www.threads.net/@islam.jahidul.jihad">
                         <BsFillThreadsFill />
                     </a>
                 </div>
